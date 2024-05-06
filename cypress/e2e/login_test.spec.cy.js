83ac574672cff39824d9de84229234b5accd587c
KaterinaTyukhanova <klimovafiona@gmail.com>
2024-05-07 00:39:16 +0700

    авторизация (часть)

---
+++ cypress/e2e/login_test.spec.cy.js
@@ -0,0 +1,20 @@
+describe('Authorization Test', () => {
+    it('Positive Login test', () => {
+        cy.fixture('cypressAuthorization').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Ввод логина')
+            cy.get(':nth-child(1) > .form-control--medium > .form-input--text').type(data.login)
+            
+            cy.log('Ввод пароля')
+            cy.get(':nth-child(2) > .form-control--medium > .form-input--password').type(data.password)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('.form__buttons > :nth-child(3)').click()
+
+            cy.log('Проверка, что пользователь успешно авторизовался (перешл в ЛК)')
+            cy.url().should('equal', 'https://dev.profteam.su/account/main')
+        });
+    });
+});
--- cypress/e2e/register_test.spec.cy.js
+++ cypress/e2e/register_test.spec.cy.js
@@ -5,29 +5,28 @@ describe('Registration Test', () => {
             cy.visit(data.main_url)
 
             cy.log('Ввод логина')
-            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium > .form-input--text').type(data.login)
+            cy.get(':nth-child(1) > :nth-child(1) > .form-control--medium .form-input--text').type(data.login)
             
             cy.log('Ввод почты')
-            cy.get('.form-input--email').type(data.email)
+            cy.get(':nth-child(1) > :nth-child(2) > .form-control--medium > .form-input--email').type(data.email)
             
             cy.log('Ввод пароля')
-            cy.get(':nth-child(3) > .form-control--medium > .form-input--password').type(data.password)
+            cy.get(':nth-child(1) > :nth-child(3) > .form-control--medium > .form-input--password').type(data.password)
 
             cy.log('Ввод подтверждения пароля')
-            cy.get(':nth-child(4) > .form-control--medium > .form-input--password').type(data.again_password)
+            cy.get(':nth-child(1) > :nth-child(4) > .form-control--medium > .form-input--password').type(data.again_password)
 
             cy.log('Клик по кнопке "Далее"')
             cy.get(':nth-child(4) > .button').click()
-
 
             cy.log('Ввод фамилии')
             cy.get('[style=""] > :nth-child(1) > .form-control--medium > .form-input--text').type(data.surname)
             
             cy.log('Ввод имени')
-            cy.get(':nth-child(2) > .form-control--medium > .form-input--text').type(data.name)
+            cy.get('[style=""] > :nth-child(2) > .form-control--medium > .form-input--text').type(data.name)
             
             cy.log('Ввод отчества')
-            cy.get(':nth-child(3) > .form-control--medium > .form-input--text').type(data.patronym)
+            cy.get('[style=""] > :nth-child(3) > .form-control--medium > .form-input--text').type(data.patronym)
 
             cy.log('Клик по кнопке "Войти"')
             cy.get(':nth-child(3) > .button').click()
